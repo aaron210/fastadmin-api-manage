76c11e026c2d1723eccf069f0fe126229e67f272
wxj0707 <wxj0707@163.com>
2020-05-17 23:52:07 +0800

    优化

--- application/url.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-   "zhuogu"=>"index/project",
-   "dingfeng"=>"index/project",
-   "zhuoguivr"=>"index/project",
-   "min"=>"index/project",
-   "weimi"=>"index/project",
-   "haohua"=>"index/project",
-];
